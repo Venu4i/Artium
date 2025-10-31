ef419ad547838489a0ef20965bccb8c29cd123bc
Shraddha Sharma <shraddhasharma1165@gmail.com>
2025-11-01 02:15:15 +0530

    add:login/sigupPage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AuthPage from './components/AuthPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <AuthPage />
+    </>
+  )
+}
+
+export default App
